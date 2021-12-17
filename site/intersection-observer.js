6980c1f95355762f0799852dfa6d3c1b1e0aa836
mashanng <92316379+mashanng@users.noreply.github.com>
2021-12-17 17:53:34 +0300

    added observers

--- site/intersection-observer.js
+++ site/intersection-observer.js
@@ -1,4 +1,4 @@
-const faders=document.querySelector('.about-section');
+/*const faders=document.querySelectorAll('.about-section');
 
 const appearOptions={
     threshold:1,
@@ -13,14 +13,42 @@ const appearOnScroll = new IntersectionObserver(function(
         if (!entry.isIntersecting){
             return;
         } else{
-            entry.target.classList.add('appear');
-            appearOnScroll.unobserve(entry.target)
+            entry.target.classList.add("appear");
+            appearOnScroll.unobserve(entry.target);
         }
-    })
+    });
 },appearOptions);
 
 faders.forEach(fader=>{
     appearOnScroll.observe(fader);
-});
+});    */
 
+window.addEventListener('DOMContentLoaded', setup);
 
+function setup (){
+    const options = {
+        rootMargin:' 0px 0px -100px 0px'
+    };
+    const observer = new IntersectionObserver((entries, observer)=>{
+        entries.forEach(entry => {
+            if(entry.isIntersecting){
+                entry.target.classList.add('show');
+                observer.unobserve(entry.target);
+            } else {
+               return;
+            }
+        });
+    },options);
+
+    const h2 = document.querySelector('h2');
+    observer.observe(h2);
+
+    const h3 = document.querySelector('h3');
+    observer.observe(h3);
+
+    const img = document.querySelectorAll('img');
+   img.forEach(img=> observer.observe(img));
+
+    const paras = document.querySelectorAll('p');
+    paras.forEach(p=>observer.observe(p));
+}
