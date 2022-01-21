da33646020089269558ad9e6c22ba96a92e64e3f
mashanng <92316379+mashanng@users.noreply.github.com>
2022-01-21 09:51:33 +0300

    addition

--- site/intersection-observer.js
+++ site/intersection-observer.js
@@ -28,3 +28,31 @@ function setup (){
     const paras = document.querySelectorAll('p');
     paras.forEach(p=>observer.observe(p));
 }
+
+/*---------------contact us ---------*/
+
+$('.contact-form').find('.form-control').each(function() {
+    var targetItem = $(this).parent();
+    if ($(this).val()) {
+      $(targetItem).find('label').css({
+        'top': '10px',
+        'fontSize': '14px'
+      });
+    }
+  })
+  $('.contact-form').find('.form-control').focus(function() {
+    $(this).parent('.input-block').addClass('focus');
+    $(this).parent().find('label').animate({
+      'top': '10px',
+      'fontSize': '14px'
+    }, 300);
+  })
+  $('.contact-form').find('.form-control').blur(function() {
+    if ($(this).val().length == 0) {
+      $(this).parent('.input-block').removeClass('focus');
+      $(this).parent().find('label').animate({
+        'top': '25px',
+        'fontSize': '18px'
+      }, 300);
+    }
+  })
